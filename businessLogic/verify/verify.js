b5df029ce9b703fc39f68fb8c4af86375f3a242e
liutsroman <liutsroman@gmail.com>
2016-04-26 18:00:56 +0300

    delete unnecessary folders and files

--- businessLogic/verify/verify.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by Roman on 26.04.2016.
- */
--- routes/verify.js
+++ routes/verify.js
@@ -13,8 +13,7 @@ exports.getToken = function (user) {
 };
 
 exports.verifyOrdinaryUser = function (req, res, next) {
-    // check header or url parameters or post parameters for token
-    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    var token = req.headers['authorization'];
 
     // decode token
     if (token) {
